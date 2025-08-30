b895193a1cfd88dfce8e644366c26c1451b4a9b5
Meoshameka Taylor <m_taylor77@me.com>
2025-08-29 21:26:52 -0500

    Restart v2

--- app.js
+++ app.js
@@ -1,49 +1,62 @@
 
-
-
-async function fetchMovies(searchTerm) 
-
-    const response = await 
-    
-        fetch (`https://www.omdbapi.com/?apikey=60623f72&s=${searchTerm}`);
-
-
-const searchResults = await response.json();
-
- const array = await searchResults.Search.slice(0, 6);
-
 const movies = document.querySelector(".movies__list");
+
+movies.innerHTML = `<i class="fa-solid fa-spinner movies__list__spinner"></i>`;
+
+let moviesData = await fetchMovies();
+
+
+// const movies.innerHTML = `<i class="fa-solid fa-spinner movies__list__spinner"></i>`;
+//   let movies = await fetchMovies();
+
+
+async function moviesSearch(value) {
+  
+  const response = await 
+  
+      fetch (`https://www.omdbapi.com/?apikey=60623f72&s=${value}`);
+      
+      const data = await response.json();
+      
+      return data;
+}
+
+
+
+const array = await searchResults.Search.slice(0, 6);
+
+const moviesList = document.querySelector(".movies__list");
 
 const moviesHTML = array
 
 
 
- .map((movies) => {
-      return `<div class="movie movie__invisible">
+.map((movies) => {
+  return `<div class="movie movie__invisible">
 <figure class="movie__img__wrapper">
-  <img src="${movie.Poster}" alt="" class="movie__img">
-  <h3 class="movie__info__title">${movie.Title}</h3>
-  <div class="movie__info__list">
-    <div class="movie__info">
-      <i class="fa-solid fa-clock movie__info__icon"></i>
-      <p class="movie__info__text">136m</p>
-    </div>
-    <div class="movie__info">
-      <i class="fa-solid fa-star movie__info__icon"></i>
-      <p class="movie__info__text">4.5</p>
-    </div>
-    <div class="movie__info">
-      <i class="fa-solid fa-earth-americas movie__info__icon"></i>
-      <p class="movie__info__text">English</p>
-    </div>
-  </div>
+<img src="${movie.Poster}" alt="" class="movie__img">
+<h3 class="movie__info__title">${movie.Title}</h3>
+<div class="movie__info__list">
+<div class="movie__info">
+  <i class="fa-solid fa-clock movie__info__icon"></i>
+  <p class="movie__info__text">136m</p>
+</div>
+<div class="movie__info">
+  <i class="fa-solid fa-star movie__info__icon"></i>
+  <p class="movie__info__text">4.5</p>
+</div>
+<div class="movie__info">
+  <i class="fa-solid fa-earth-americas movie__info__icon"></i>
+  <p class="movie__info__text">English</p>
+</div>
+</div>
 </figure>
 <h4 class="movie__title">${movie.Title}</h4>
 </div>`;
-    })
-    .join('');
+})
+.join('');
 
 
- moviesList.innerHTML = moviesHTML;
+moviesList.innerHTML = moviesHTML;
 
 ``
